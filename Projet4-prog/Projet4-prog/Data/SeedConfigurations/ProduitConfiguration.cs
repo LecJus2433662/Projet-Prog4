672e823fef2588e2b86c063f69b529baa2fe59b9
Oleksandr Bilynets <2431570@etu.cegepjonquiere.ca>
2026-05-17 17:18:53 -0400

    page stripe/ jsp fait ou pas

--- Projet4-prog/Projet4-prog/Data/SeedConfigurations/ProduitConfiguration.cs
+++ Projet4-prog/Projet4-prog/Data/SeedConfigurations/ProduitConfiguration.cs
@@ -4,21 +4,23 @@ using Projet4_prog.Models;
 
 namespace Projet4_prog.Data.SeedConfigurations
 {
-    public class ProduitConfiguration: IEntityTypeConfiguration<Produit>
+    public class ProduitConfiguration : IEntityTypeConfiguration<Produit>
     {
         public void Configure(EntityTypeBuilder<Produit> builder)
         {
             builder.HasData(
 
-                new Produit { 
+                new Produit
+                {
                     Id = 1,
                     Nom = "Bacon halal au sirop d'érable",
-                    Description= "bacon infusé au sirop d'érable cuisiner avec l'amour pour Jad",
-                    Prix = 12.67 ,
+                    Description = "bacon infusé au sirop d'érable cuisiner avec l'amour pour Jad",
+                    Prix = 12.67,
                     NbProduitRestant = 67,
                     Image = "https://images.unsplash.com/photo-1742859052497-f8bbc8366a32?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmFjb258ZW58MHx8MHx8fDA%3D"
                 },
-                new Produit { 
+                new Produit
+                {
                     Id = 2,
                     Nom = "Pudding chomeur",
                     Description = "c'est tellement bon pour mathis",
@@ -26,7 +28,8 @@ namespace Projet4_prog.Data.SeedConfigurations
                     NbProduitRestant = 67,
                     Image = "https://images.ricardocuisine.com/services/recipes/992x1340_8894.jpg"
                 },
-                new Produit {
+                new Produit
+                {
                     Id = 3,
                     Nom = "Tire d'érable",
                     Description = "Le sirop d'érable chauffé est versé dans la neige pour créer la tire. · Versez le sirop d'érable dans une neige compactée",
@@ -42,9 +45,43 @@ namespace Projet4_prog.Data.SeedConfigurations
                     Prix = 8.67,
                     NbProduitRestant = 67,
                     Image = "https://img.fourchette-et-bikini.fr/1200x900/2025/03/07/i50846-crepes-sucrees-sans-gluten-et-faciles.webp"
+                },
+                new Produit
+                {
+                    Id = 5,
+                    Nom = "Beurre d'érable",
+                    Description = "Une tartinade sucrée et crémeuse faite 100% de sirop d'érable pur",
+                    Prix = 9.99,
+                    NbProduitRestant = 45,
+                    Image = "https://images.squarespace-cdn.com/content/v1/5ff4c7c50debdf5d1ff004bc/93f4f384-792c-495c-97b3-fdca6c514eb9/Beurre+d%27%C3%A9rable.png?format=1500w"
+                },
+                new Produit
+                {
+                    Id = 6,
+                    Nom = "Cornet à l'érable",
+                    Description = "Crème glacée à la vanille recouverte de sirop d'érable chaud",
+                    Prix = 6.49,
+                    NbProduitRestant = 32,
+                    Image = "https://cdn.pratico-pratiques.com/app/uploads/sites/3/2018/08/20185844/mini-cornets-a-l-erable.jpeg"
+                },
+                new Produit
+                {
+                    Id = 7,
+                    Nom = "Biscuits à l'érable",
+                    Description = "Biscuits croustillants fourrés avec une crème au sirop d'érable",
+                    Prix = 5.99,
+                    NbProduitRestant = 78,
+                    Image = "https://erableduquebec.ca/uploads/2022/01/recette-biscuits-erable-1200x900-1-600x450.jpg"
+                },
+                new Produit
+                {
+                    Id = 8,
+                    Nom = "Lait frappé à l'érable",
+                    Description = "Un milkshake froid et sucré avec du vrai sirop d'érable du Québec",
+                    Prix = 7.25,
+                    NbProduitRestant = 21,
+                    Image = "https://erableduquebec.ca/uploads/2022/07/recette-lait-frappe-erable-1200x900-1-600x450.jpg"
                 }
-
-
             );
         }
     }
---
+++ Projet4-prog/Projet4-prog/Migrations/20260517191710_produitsFull.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Projet4_prog.Migrations
+{
+    /// <inheritdoc />
+    public partial class produitsFull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Produits",
+                columns: new[] { "Id", "Description", "Image", "NbProduitRestant", "Nom", "Prix" },
+                values: new object[,]
+                {
+                    { 5, "Une tartinade sucrée et crémeuse faite 100% de sirop d'érable pur", "https://images.squarespace-cdn.com/content/v1/5ff4c7c50debdf5d1ff004bc/93f4f384-792c-495c-97b3-fdca6c514eb9/Beurre+d%27%C3%A9rable.png?format=1500w", 45, "Beurre d'érable", 9.9900000000000002 },
+                    { 6, "Crème glacée à la vanille recouverte de sirop d'érable chaud", "https://cdn.pratico-pratiques.com/app/uploads/sites/3/2018/08/20185844/mini-cornets-a-l-erable.jpeg", 32, "Cornet à l'érable", 6.4900000000000002 },
+                    { 7, "Biscuits croustillants fourrés avec une crème au sirop d'érable", "https://erableduquebec.ca/uploads/2022/01/recette-biscuits-erable-1200x900-1-600x450.jpg", 78, "Biscuits à l'érable", 5.9900000000000002 },
+                    { 8, "Un milkshake froid et sucré avec du vrai sirop d'érable du Québec", "https://erableduquebec.ca/uploads/2022/07/recette-lait-frappe-erable-1200x900-1-600x450.jpg", 21, "Lait frappé à l'érable", 7.25 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Produits",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Produits",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Produits",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Produits",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
